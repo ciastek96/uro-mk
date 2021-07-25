d3bb6227dc05ae7b9feb84ac441f439c7f5459f9
unknown <ciastek1996@gmail.com>
2021-07-25 21:28:15 +0200

    finished hamburger component

--- components/Hamburger/Hamburger.js
+++ components/Hamburger/Hamburger.js
@@ -1,15 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  width: 48px;
-  height: 48px;
+const Wrapper = styled.button`
+  padding: 10px;
   cursor: pointer;
-  background: red;
+  background: transparent;
+  border: 0;
+  margin: 0;
+  display: inline-block;
+`;
+
+const Box = styled.span`
+  width: 35px;
+  height: 24px;
+  display: inline-block;
+  position: relative;
+`;
+
+const InnerWrapper = styled.span`
+  width: 100%;
+  height: 3px;
+  background-color: ${({ isOpen }) => (isOpen ? "transparent" : "white")};
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+  transition: background-color 0.2s linear;
+
+  &::after,
+  &::before {
+    content: "";
+    width: 100%;
+    height: 3px;
+    position: absolute;
+    background-color: white;
+    transition: all 0.2s linear;
+  }
+
+  &::after {
+    top: ${({ isOpen }) => (isOpen ? "0" : "-10px")};
+    right: 0;
+    transform: ${({ isOpen }) => (isOpen ? "rotateZ(45deg)" : "rotateZ(0)")};
+  }
+
+  &::before {
+    top: ${({ isOpen }) => (isOpen ? "0" : "10px")};
+    right: 0;
+    transform: ${({ isOpen }) => (isOpen ? "rotateZ(-45deg)" : "rotateZ(0)")};
+  }
 `;
 
 const Hamburger = () => {
-  return <Wrapper></Wrapper>;
+  const [isToggle, setIsToggle] = useState(false);
+
+  return (
+    <Wrapper onClick={() => setIsToggle((isToggle) => !isToggle)}>
+      <Box>
+        <InnerWrapper isOpen={isToggle}></InnerWrapper>
+      </Box>
+    </Wrapper>
+  );
 };
 
 export default Hamburger;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,12 @@ export default function Home() {
       <div className={styles.container}>
         <Head>
           <title>URO-MK - Usługi remontowo ociepleniowe MK Marek Kołacz</title>
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <meta
             name="description"
             content="Usługi remontowo ociepleniowe MK Marek Kołacz"
