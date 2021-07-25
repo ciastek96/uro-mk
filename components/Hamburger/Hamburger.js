9a1804eeff4017f3ec90ae1bd3bbe7ea968bfb14
unknown <ciastek1996@gmail.com>
2021-07-25 18:17:47 +0200

    added icons, svgs and created Header

---
+++ components/Hamburger/Hamburger.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 48px;
+  height: 48px;
+  cursor: pointer;
+  background: red;
+`;
+
+const Hamburger = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default Hamburger;
